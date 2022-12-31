aeaad3db20deb0137b1ba2d4b56e991a7f4626ff
AKD671 <84254783+AKD671@users.noreply.github.com>
2022-12-31 07:29:14 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fpjzzub51_)hhky^jmnl1g4d7ncp+)2&)8p8gijny_$3#%otwc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
